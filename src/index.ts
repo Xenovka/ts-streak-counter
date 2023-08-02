d836910f308b2c1d8b4a14660c7ec33a1d129bf4
Xenovka <wendyanto18@gmail.com>
2023-08-02 09:11:56 +0700

    refactoring and more testing

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,63 @@
-import { formattedDate } from "./utils";
+import { buildStreak, formattedDate, KEY, Streak, updateStreak } from "./utils";
 
-interface Streak {
-  currentCount: number;
-  startDate: string;
-  lastLoginDate: string;
+export function differenceInDays(dateLeft: Date, dateRight: Date): number {
+    const diffTime = Math.abs(dateLeft.getTime() - dateRight.getTime());
+    const differenceInDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    return differenceInDays;
 }
 
-const KEY = "streak";
+function shouldIncrementOrResetStreakCount(currentDate: Date, lastLoginDate: string): "increment" | "reset" | "none" {
+    const difference = differenceInDays(currentDate, new Date(lastLoginDate));
+    if (difference === 0) {
+        return "none";
+    }
+
+    if (difference === 1) {
+        return "increment";
+    }
+
+    return "reset";
+}
 
 export function streakCounter(storage: Storage, date: Date): Streak {
-  const streakInLocalStorage = storage.getItem(KEY);
-  if (streakInLocalStorage) {
-    try {
-      const streak = JSON.parse(streakInLocalStorage || "");
-      return streak;
-    } catch (error) {
-      console.error("Failed to parse streak from localStorage");
+    const streakInLocalStorage = storage.getItem(KEY);
+    if (streakInLocalStorage) {
+        try {
+            const streak = JSON.parse(streakInLocalStorage) as Streak;
+            const state = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate);
+            const SHOULD_INCREMENT = state === "increment";
+            const SHOULD_RESET = state === "reset";
+
+            if (SHOULD_INCREMENT) {
+                const updatedStreak: Streak = buildStreak(date, {
+                    startDate: streak.startDate,
+                    currentCount: streak.currentCount + 1,
+                    lastLoginDate: formattedDate(date)
+                });
+
+                storage.setItem(KEY, JSON.stringify(updatedStreak));
+
+                return updatedStreak;
+            }
+
+            if (SHOULD_RESET) {
+                const updatedStreak: Streak = buildStreak(date);
+
+                updateStreak(storage, updatedStreak);
+
+                return updatedStreak;
+            }
+
+            return streak;
+        } catch (error) {
+            console.error("Failed to parse streak from localStorage");
+        }
     }
-  }
 
-  const streak = {
-    currentCount: 1,
-    startDate: formattedDate(date),
-    lastLoginDate: formattedDate(date)
-  };
+    const streak = buildStreak(date);
 
-  storage.setItem(KEY, JSON.stringify(streak));
+    storage.setItem(KEY, JSON.stringify(streak));
 
-  return streak;
+    return streak;
 }
